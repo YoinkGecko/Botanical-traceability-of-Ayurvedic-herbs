8ae04f3f5c0ee7618c9193f4be5701b84fbeb3b4
Kartikeya <kartikeya.anjul@gmail.com>
2025-09-06 21:50:09 +0530

    added app

---
+++ twentyseven/app/src/main/java/com/example/twentyseven/loginscreen.java
@@ -0,0 +1,32 @@
+package com.example.twentyseven;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class loginscreen extends AppCompatActivity {
+
+    EditText phonenumber,otp;
+    Button sendotp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login_screen);
+        phonenumber = findViewById(R.id.phonenumber);
+        otp = findViewById(R.id.otp);
+        sendotp = findViewById(R.id.sentotp);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
