cdd2a2c02b7bfc1554ed20578a58362499b34d29
Kartikeya <kartikeya.anjul@gmail.com>
2025-09-15 15:17:37 +0530

    24-12 fromat

--- admin_site/src/pages/supply-chain-overview-dashboard/index.jsx
+++ admin_site/src/pages/supply-chain-overview-dashboard/index.jsx
@@ -645,7 +645,7 @@ const SupplyChainOverviewDashboard = () => {
       hour: "2-digit",
       minute: "2-digit",
       second: "2-digit",
-      hour12: false,
+      hour12: true,
     });
   };
 
