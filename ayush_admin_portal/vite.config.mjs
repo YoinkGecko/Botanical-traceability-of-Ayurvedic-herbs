65ae8efde05a7450690ae03d5206424913d4c3bd
Kartikeya <kartikeya.anjul@gmail.com>
2026-04-05 22:06:37 +0530

    port changed

--- ayush_admin_portal/vite.config.mjs
+++ ayush_admin_portal/vite.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4029",
+    port: "1000",
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
